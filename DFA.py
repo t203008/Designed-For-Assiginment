7205d72ef72153db5a4fb11bc9b33f0d45962ef4
Ryosuke Ohtsuki <91816208+t203008@users.noreply.github.com>
2021-10-15 15:53:12 +0900

    仮

--- DFA.py
+++ DFA.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import pandas as pd
-import yellowbrick
-from warnings import simplefilter
-from sklearn.exceptions import ConvergenceWarning
-simplefilter("ignore", category=ConvergenceWarning)
-simplefilter(action='ignore', category=FutureWarning)
 
 dfa=pd.read_csv("戦力外.csv")
-X=dfa.drop("DFA",axis=1)
-st.write(X)
+from pycaret.regression import *
+reg=setup(gpa,target="GPA")
+st.write(reg)
