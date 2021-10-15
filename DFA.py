938045668b455d570fcc781ad58dee42db14d5a5
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-15 15:02:07 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
-import pycaret
+import yellowbrick
+from warnings import simplefilter
+from sklearn.exceptions import ConvergenceWarning
+simplefilter("ignore", category=ConvergenceWarning)
+simplefilter(action='ignore', category=FutureWarning)
+
 dfa=pd.read_csv("戦力外.csv")
-reg=setup(dfa,target="DFA")
-st.write(reg)
+X=dfa.drop("DFA",axis=1)
+st.write(X)
