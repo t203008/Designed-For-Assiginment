e43990fdc0af26f2bd62e0759f3240c96b61aa05
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-17 05:32:50 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -15,23 +15,6 @@ dfa=dfa.astype(float)
 y=dfa["DFA"]
 X=dfa.drop(["DFA","名前"],axis=1)
 
-from sklearn.model_selection import train_test_split 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-from sklearn.linear_model import LogisticRegression
-logreg=LogisticRegression()
-logreg.fit(X_train,y_train)
-y_pred = logreg.predict(X_test)
-
-from sklearn import metrics
-st.write(metrics.accuracy_score(y_test,y_pred))
-
-dfa=pd.read_csv("戦力外.csv")
-dfa = pd.get_dummies(dfa, drop_first=True) 
-dfa=dfa.fillna(0)
-dfa=dfa.astype(float)
-y=dfa["DFA"]
-X=dfa.drop(["DFA","名前"],axis=1)
 
 from yellowbrick.classifier import ConfusionMatrix
 logreg=LogisticRegression()
@@ -48,3 +31,15 @@ visualizer = ROCAUC(logreg, size=(600,400))
 visualizer.fit(X, y)
 visualizer.score(X, y)
 st.write(visualizer)
+
+
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+from sklearn.linear_model import LogisticRegression
+logreg=LogisticRegression()
+logreg.fit(X_train,y_train)
+y_pred = logreg.predict(X_test)
+
+from sklearn import metrics
+st.write(metrics.accuracy_score(y_test,y_pred))
