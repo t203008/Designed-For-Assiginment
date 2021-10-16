a72075f6689299f135483c0a1e46b46c413baacb
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-16 18:48:44 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -9,10 +9,6 @@ simplefilter(action='ignore', category=FutureWarning)
 
 dfa=pd.read_csv("戦力外.csv")
 y=dfa["DFA"]
-X = dfa.drop("DFA",axis=1)
-
-from sklearn.linear_model import LinearRegression
-reg = LinearRegression()
-reg.fit(X, y)
-yhat = reg.predict(X)
-st.write(reg.score(X,y))
+X =dfa.drop("DFA",axis=1)
+st.write(y)
+st.write(X
