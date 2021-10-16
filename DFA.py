fe895e6f397165c280035234a56e5ef2bbb07c7d
Ryosuke Ohtsuki <91816208+t203008@users.noreply.github.com>
2021-10-16 18:44:00 +0900

    仮

--- DFA.py
+++ DFA.py
@@ -1,11 +1,31 @@
 import streamlit as st
 import pandas as pd
 import yellowbrick
+
 from warnings import simplefilter
 from sklearn.exceptions import ConvergenceWarning
 simplefilter("ignore", category=ConvergenceWarning)
 simplefilter(action='ignore', category=FutureWarning)
 
 dfa=pd.read_csv("戦力外.csv")
-st.write(dfa.head())
+y=dfa["DFA"]
+X = dfa.drop("DFA",axis=1)
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(X, y)
+yhat = reg.predict(X)
+
+from yellowbrick.regressor import PredictionError
+
+visualizer = PredictionError(reg)
+visualizer.fit(X, y)  
+visualizer.score(X, y)  
+st.write(visualizer)
+
+from yellowbrick.regressor import ResidualsPlot
+
+visualizer = ResidualsPlot(reg)
+visualizer.fit(X, y)  
+visualizer.score(X, y)  
+st.write(visualizer)
