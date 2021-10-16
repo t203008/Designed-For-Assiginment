4fb7e796361bd1fcb41b2b8e2f0b7fc82d86d1c0
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-16 17:41:56 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -1,10 +1,4 @@
 import streamlit as st
 import pandas as pd
-import pycaret
-
-dfa=pd.read_csv("戦力外.csv")
-from pycaret.regression import *
-reg=setup(dfa,target="DFA")
-best_model = compare_models(fold=5)
-predict_model(best_model)
-st.write(plot_model(best_model))
+import yellowbrick
+st.write(yellowbrick.__version__)
