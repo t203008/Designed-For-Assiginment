aaf1c970d965ad2cc5a1af63ceece977b5938511
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-16 18:55:53 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -8,17 +8,9 @@ simplefilter("ignore", category=ConvergenceWarning)
 simplefilter(action='ignore', category=FutureWarning)
 
 dfa=pd.read_csv("戦力外.csv")
+
+dfa = pd.get_dummies(dfa, drop_first=True) 
+
 y=dfa["DFA"]
 X =dfa.drop("DFA",axis=1)
-
-from sklearn.linear_model import LogisticRegression
-logreg =  LogisticRegression()
-logreg.fit(X,y)
-
-from yellowbrick.classifier import ConfusionMatrix
-
-cm = ConfusionMatrix(logreg, classes=["not dfa", "is dfa"])
-
-cm.fit(X, y)
-cm.score(X, y)
-st.write(cm)
+st.write(X)
