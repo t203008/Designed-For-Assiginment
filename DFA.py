86c63ba0635811f741688bd7be35c2c77bfb7556
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-17 05:22:29 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -26,6 +26,13 @@ y_pred = logreg.predict(X_test)
 from sklearn import metrics
 st.write(metrics.accuracy_score(y_test,y_pred))
 
+dfa=pd.read_csv("戦力外.csv")
+dfa = pd.get_dummies(dfa, drop_first=True) 
+dfa=dfa.fillna(0)
+dfa=dfa.astype(float)
+y=dfa["DFA"]
+X=dfa.drop(["DFA","名前"],axis=1)
+
 from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor()
 rf.fit(X, y)
@@ -34,12 +41,7 @@ from sklearn.inspection import permutation_importance
 result = permutation_importance(rf, X, y)
 st.write(result.importances_mean)
 
-#dfa=pd.read_csv("戦力外.csv")
-#dfa = pd.get_dummies(dfa, drop_first=True) 
-#dfa=dfa.fillna(0)
-#dfa=dfa.astype(float)
-#y=dfa["DFA"]
-#X=dfa.drop(["DFA","名前"],axis=1)
+
 #from sklearn.linear_model import LogisticRegression
 #logreg =  LogisticRegression()
 #logreg.fit(X, y)
