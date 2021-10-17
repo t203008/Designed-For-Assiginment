eb840b0f990eb8695cbcbc186f13323624367cae
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-17 13:34:01 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -9,15 +9,15 @@ simplefilter(action='ignore', category=FutureWarning)
 st.title("戦力外予想")
 
 train=int(st.sidebar.number_input('訓練データ量[%]',0,100,30))
-batter=st.sidebar.text_input("打撃データ(NPBのデータをコピペ)",)
-pitcher=st.sidebar.text_input("投手データ(NPBのデータをコピペ)",)
-pf=st.sidebar.text_input("投手での守備データ(NPBのデータの守備率前までをコピペ)",)
-cf=st.sidebar.text_input("捕手での守備データ(NPBのデータの守備率前までをコピペ)",)
-fbf=st.sidebar.text_input("一塁手での守備データ(NPBのデータの守備率前までをコピペ)",)
-sbf=st.sidebar.text_input("二塁手での守備データ(NPBのデータの守備率前までをコピペ)",)
-tbf=st.sidebar.text_input("三塁手での守備データ(NPBのデータの守備率前までをコピペ)",)
-ssf=st.sidebar.text_input("遊撃手での守備データ(NPBのデータの守備率前までをコピペ)",)
-off=st.sidebar.text_input("外野での守備データ(NPBのデータの守備率前までをコピペ)",)
+batter=[st.sidebar.text_input("打撃データ(NPBのデータをコピペ)",)]
+pitcher=[st.sidebar.text_input("投手データ(NPBのデータをコピペ)",)]
+pf=[st.sidebar.text_input("投手での守備データ(NPBのデータの守備率前までをコピペ)",)]
+cf=[st.sidebar.text_input("捕手での守備データ(NPBのデータの守備率前までをコピペ)",)]
+fbf=[st.sidebar.text_input("一塁手での守備データ(NPBのデータの守備率前までをコピペ)",)]
+sbf=[st.sidebar.text_input("二塁手での守備データ(NPBのデータの守備率前までをコピペ)",)]
+tbf=[st.sidebar.text_input("三塁手での守備データ(NPBのデータの守備率前までをコピペ)",)]
+ssf=[st.sidebar.text_input("遊撃手での守備データ(NPBのデータの守備率前までをコピペ)",)]
+off=[st.sidebar.text_input("外野での守備データ(NPBのデータの守備率前までをコピペ)",)]
 
 dfa=pd.read_csv("戦力外.csv")
 dfa = pd.get_dummies(dfa, drop_first=True) 
