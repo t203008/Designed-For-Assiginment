131dfbede9773757c5c374d11ddb8ab519165ec2
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-17 14:49:18 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -86,6 +86,6 @@ if len(off)==0:
 
 fielding=[]
 for n in off:
-  fielding+=pf[n]+cf[n]+fbf[n]+sbf[n]+tbf[n]+ssf[n]+off[n]
+  fielding.append(pf[n]+cf[n]+fbf[n]+sbf[n]+tbf[n]+ssf[n]+off[n])
 
 st.write(fielding)
