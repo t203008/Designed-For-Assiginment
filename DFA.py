0ff4f4c1d6c999d3497bd1f1b737490e914b5aea
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-17 12:54:27 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -21,6 +21,7 @@ X=dfa.drop("DFA",axis=1)
 from sklearn.model_selection import train_test_split 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=train/100)
 
+from sklearn.linear_model import LogisticRegression
 logreg=LogisticRegression()
 logreg.fit(X_train,y_train)
 y_pred = logreg.predict(X_test)
