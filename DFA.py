63b173f93cdb5690c276c90fecb4d4703c1273cc
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-17 13:48:49 +0900

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -8,17 +8,6 @@ simplefilter("ignore", category=ConvergenceWarning)
 simplefilter(action='ignore', category=FutureWarning)
 st.title("戦力外予想")
 
-train=int(st.sidebar.number_input('訓練データ量[%]',0,100,30))
-batter=[st.sidebar.text_input("打撃データ(NPBのデータをコピペ)",)]
-pitcher=[st.sidebar.text_input("投手データ(NPBのデータをコピペ)",)]
-pf=[st.sidebar.text_input("投手での守備データ(NPBのデータの守備率前までをコピペ)",)]
-cf=[st.sidebar.text_input("捕手での守備データ(NPBのデータの守備率前までをコピペ)",)]
-fbf=[st.sidebar.text_input("一塁手での守備データ(NPBのデータの守備率前までをコピペ)",)]
-sbf=[st.sidebar.text_input("二塁手での守備データ(NPBのデータの守備率前までをコピペ)",)]
-tbf=[st.sidebar.text_input("三塁手での守備データ(NPBのデータの守備率前までをコピペ)",)]
-ssf=[st.sidebar.text_input("遊撃手での守備データ(NPBのデータの守備率前までをコピペ)",)]
-off=[st.sidebar.text_input("外野での守備データ(NPBのデータの守備率前までをコピペ)",)]
-
 dfa=pd.read_csv("戦力外.csv")
 dfa = pd.get_dummies(dfa, drop_first=True) 
 dfa=dfa.fillna(0)
@@ -37,3 +26,19 @@ y_pred = logreg.predict(X_test)
 
 from sklearn import metrics
 st.write(metrics.accuracy_score(y_test,y_pred))
+
+
+train=int(st.sidebar.number_input('訓練データ量[%]',0,100,30))
+batter=st.sidebar.text_input("打撃データ(NPBのデータの試合から出塁率までをコピペ)",)
+pitcher=st.sidebar.text_input("投手データ(NPBのデータの登板から防御率をコピペ)",)
+pf=st.sidebar.text_input("投手での守備データ(NPBのデータの試合から併殺までをコピペ)",)
+cf=st.sidebar.text_input("捕手での守備データ(NPBのデータの試合から捕逸までをコピペ)",)
+fbf=st.sidebar.text_input("一塁手での守備データ(NPBのデータの試合から併殺までをコピペ)",)
+sbf=st.sidebar.text_input("二塁手での守備データ(NPBのデータの試合から併殺までをコピペ)",)
+tbf=st.sidebar.text_input("三塁手での守備データ(NPBのデータの試合から併殺までをコピペ)",)
+ssf=st.sidebar.text_input("遊撃手での守備データ(NPBのデータの試合から併殺までをコピペ)",)
+off=st.sidebar.text_input("外野での守備データ(NPBのデータの試合から併殺までをコピペ)",)
+
+b=batter.split()
+b=[float(n) for n in b]
+st.write(b)
